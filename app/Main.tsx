d87f62c437abe01a9ea53b9b0b9c354b3629ff2b
blackcoffeexbt <87530449+blackcoffeexbt@users.noreply.github.com>
2025-05-15 09:10:01 +0100

    Remove divide

--- app/Main.tsx
+++ app/Main.tsx
@@ -45,7 +45,7 @@ export default async function Main({ searchParams }: MainProps) {
 
   return (
     <>
-      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+      <div>
         <div className="prose space-y-2 pb-8 pt-6 dark:prose-invert md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             LNbits News
